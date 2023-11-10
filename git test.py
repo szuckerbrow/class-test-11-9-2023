90a728e6ba91c776342eee2f8a3c9453dbbdf993
szuckerbrow <146024427+szuckerbrow@users.noreply.github.com>
2023-11-09 21:53:37 -0500

    First commit

---
+++ git test.py
@@ -0,0 +1,16 @@
+# @TODO: Write a function that returns the arithmetic average for a list of numbers
+
+def average(numbers):
+    length = len(numbers)
+    total = 0
+    for number in numbers:
+        total += number
+    return total / length
+        
+
+
+
+    
+# Test your function with the following:
+print(average([1, 5, 9, 2, 2, 2]))
+print(average(range(20)))
